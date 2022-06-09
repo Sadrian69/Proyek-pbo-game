7a7ef37551cca94656211071f7a8dfbb4550b53a
Sadrian69 <adriansteven92@gmail.com>
2022-06-09 18:40:17 +0700

    Create Enemy.java

---
+++ core/src/com/mygdx/game/Enemy.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Enemy extends Rectangle {
+   private int chargeLimit;
+   private int curCharge;
+   public void move(){
+      // nanti deh pakai lerp
+   }
+}
