4be30e6d82f0b47229d5aafdb81fa97d4bf40835
felixjc1 <107231388+felixjc1@users.noreply.github.com>
2022-06-18 11:36:56 +0700

    Update EnemyRanged.java

--- core/src/com/mygdx/game/EnemyRanged.java
+++ core/src/com/mygdx/game/EnemyRanged.java
@@ -3,14 +3,46 @@ package com.mygdx.game;
 public class EnemyRanged extends Enemy{
    // menyerang hero dari mana saja
    // menyerang setiap 5 giliran
+   
+   private int distanceOffset = 300;
+
+   public EnemyRanged(Hero hero) {
+      this.width = 300;
+      this.height = 300;
+      chargeLimit = 5;
+      curCharge = 0;
+      curPos = 7;
+      lastAttack = -100f;
+      this.x = distanceOffset + pos * curPos;
+      this.y = 300;
+      this.hero = hero;
+      
+      deathSound = Gdx.audio.newSound(Gdx.files.internal("Sounds/monsterDeath.wav"));
+      
+      idling = new TextureAtlas(Gdx.files.internal("Sprites/Ranged/Idle.atlas"));
+      Array<TextureAtlas.AtlasRegion> idlingFrames = idling.findRegions("idling");
+      idleAnimation = new Animation(FRAME_DURATION, idlingFrames, com.badlogic.gdx.graphics.g2d.Animation.PlayMode.LOOP);
+
+      attacking = new TextureAtlas(Gdx.files.internal("Sprites/Ranged/Attack.atlas"));
+      Array<TextureAtlas.AtlasRegion> attackingFrames = attacking.findRegions("attacking");
+      attackAnimation = new Animation(FRAME_DURATION, attackingFrames, Animation.PlayMode.NORMAL);
+
+      firstTexture = idlingFrames.first();
+
+   }
 
    @Override
    public void move(float time) {
+      curPos--;
+      this.x = distanceOffset + curPos * pos;
+      act(time);
 
    }
 
    @Override
    public void attack(float time) {
+      if(curPos == 1 && !hero.isBlocking()) hero.Hurt();
+      lastAttack = time;
 
    }
 }
